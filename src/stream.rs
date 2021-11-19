a2a98632afc3346811049656961f22c6a2b61be8
Anton <singaraiona@gmail.com>
2021-11-19 12:09:45 +0200

    get rid on mpmc support for futures Stream because of lack an optimal correct implementation of multiple wakers queue

--- src/stream.rs
+++ src/stream.rs
@@ -9,14 +9,14 @@ use std::sync::Arc;
 
 pub struct Sender<T: Send + 'static> {
     tx: channel::Sender<T>,
-    wakers: channel::Receiver<Arc<AtomicWaker>>,
+    rx_waker: Arc<AtomicWaker>,
 }
 
 impl<T: Send + 'static> Clone for Sender<T> {
     fn clone(&self) -> Self {
         Sender {
             tx: self.tx.clone(),
-            wakers: self.wakers.clone(),
+            rx_waker: self.rx_waker.clone(),
         }
     }
 }
@@ -44,9 +44,7 @@ where
 
     fn poll_close(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.tx.close();
-        while let Ok(Some(waker)) = self.wakers.try_recv() {
-            waker.wake();
-        }
+        self.rx_waker.wake();
         Poll::Ready(Ok(()))
     }
 }
@@ -54,17 +52,6 @@ where
 pub struct Receiver<T: Send + 'static> {
     rx: channel::Receiver<T>,
     waker: Arc<AtomicWaker>,
-    wakers: channel::Sender<Arc<AtomicWaker>>,
-}
-
-impl<T: Send + 'static> Clone for Receiver<T> {
-    fn clone(&self) -> Self {
-        Receiver {
-            rx: self.rx.clone(),
-            waker: Arc::new(AtomicWaker::new()),
-            wakers: self.wakers.clone(),
-        }
-    }
 }
 
 impl<T: Send + 'static> Receiver<T> {
@@ -83,19 +70,12 @@ impl<T: Send + 'static> Stream for Receiver<T> {
     type Item = Result<T, io::Error>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        loop {
-            // It's not our event, wait until our waker is waked
-            if Arc::strong_count(&self.waker) > 1 {
-                return Poll::Pending;
+        match self.next_message() {
+            Poll::Pending => {
+                self.waker.register(cx.waker());
+                self.next_message()
             }
-
-            match self.next_message() {
-                Poll::Ready(res) => return Poll::Ready(res),
-                Poll::Pending => {
-                    self.waker.register(cx.waker());
-                    self.wakers.send(self.waker.clone())?;
-                }
-            }
+            res => res,
         }
     }
 }
