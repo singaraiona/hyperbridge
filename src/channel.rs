f5a04fb0d1f87b9429dabaa0bd7cfd4dc3819fb2
Anton <singaraiona@gmail.com>
2022-07-02 20:09:15 +0300

    close flag handle after channel is empty

--- src/channel.rs
+++ src/channel.rs
@@ -268,21 +268,27 @@ impl<T: Send> Channel<T> {
         loop {
             let head: Position<T> = Position::unpack(head_packed);
 
-            // channel is closed
-            if head.flags & CLOSED_FLAG == CLOSED_FLAG {
-                return Err(io::Error::new(
-                    io::ErrorKind::BrokenPipe,
-                    "channel is closed",
-                ));
-            }
-
             // next block is not set up yet
             if head.index == BLOCK_SIZE {
+                // channel is closed
+                if head.flags & CLOSED_FLAG == CLOSED_FLAG {
+                    return Err(io::Error::new(
+                        io::ErrorKind::BrokenPipe,
+                        "channel is closed",
+                    ));
+                }
                 return Ok(None);
             }
 
             let slot = head.slot();
             if slot.state.load(Acquire) & OCCUPIED == 0 {
+                // channel is closed
+                if head.flags & CLOSED_FLAG == CLOSED_FLAG {
+                    return Err(io::Error::new(
+                        io::ErrorKind::BrokenPipe,
+                        "channel is closed",
+                    ));
+                }
                 return Ok(None);
             }
 
